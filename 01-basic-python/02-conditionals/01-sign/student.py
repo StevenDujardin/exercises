f9a9a2b590149cc82a544fc6a3cccbf8808f5fcf
StevenDujardin <54889161+StevenDujardin@users.noreply.github.com>
2021-02-16 10:50:55 +0100

    steven

--- 01-basic-python/02-conditionals/01-sign/student.py
+++ 01-basic-python/02-conditionals/01-sign/student.py
@@ -1,1 +1,8 @@
 # Write your code here
+def sign(x):
+    if n > o:
+        return 1
+    elif n < 0:
+        return -1
+    else:
+        return 0 
--- 01-basic-python/04-lists/04-median/student.py
+++ 01-basic-python/04-lists/04-median/student.py
@@ -1,1 +1,8 @@
 # Write your code here
+def median(ns):
+    ns2 = sorted(ns)
+    i = len(ns)//2
+    if len(ns2) % 2 == 0:
+        return (ns2[i-1] + ns2[i]) / 2
+    else:
+        return ns2[i]
